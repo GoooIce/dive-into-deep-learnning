17ffa24608ab6e6cb4cdaedc9060dcd21c71166f
goooice <devel0per1991@outlook.com>
2024-03-16 12:29:21 +0800

    feat: add linear algebra mean

--- chapter-1-candle/src/linear_algebra.rs
+++ chapter-1-candle/src/linear_algebra.rs
@@ -62,17 +62,25 @@ impl LinearAlgebra {
     /// # 降维
     /// 
     /// ```
-    /// # use candle_core::{Device, Result, Tensor};
+    /// # use candle_core::{Device, Result, Tensor, NdArray};
     /// # fn main() -> Result<()> {
     /// let x = Tensor::arange(0f32, 12f32, &Device::Cpu)?.reshape(&[3, 4])?; // [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
     /// let sum = &x.sum(0)?; // [12, 15, 18, 21]
     /// # assert_eq!(sum.shape().dims(), [4]);
     /// let sum_1 = &x.sum_all()?; // [66]
     /// # assert_eq!(sum_1.shape().elem_count(), 1);
+    /// let mean = &x.mean(0)?; // [4, 5, 6, 7]
+    /// # assert_eq!(mean.to_vec1::<f32>()?, vec![4f32, 5f32, 6f32, 7f32]);
+    /// let mean_1 = &x.mean_all()?; // [5.5]
+    /// let el = Tensor::new(&[x.shape().elem_count() as f32], &x.device())?;
+    /// let mean_2 = sum_1.broadcast_div(&el)?; // [5.5]
+    /// # assert_eq!(mean_1.to_scalar::<f32>()?, 5.5f32);
+    /// # assert_eq!(mean_2.to_vec1::<f32>()?, vec![5.5f32]);
     /// # Ok(())
     /// # }
+    /// ```
     pub fn dimensionality_reduction() {
-        unimplemented!()
+        // Empty implementation
     }
 }
 
@@ -92,12 +100,22 @@ mod tests {
         println!("{:?}", transposed.to_vec2::<f32>()?); // [[1, 3], [2, 4]]
 
         let x = Tensor::arange(0f32, 12f32, &d)?.reshape(&[3, 4])?; // [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]
-        println!("{:?}", &x.shape()); // [3, 4]
+        println!("{:?}", &x); // [3, 4]
         let sum = &x.sum_keepdim(0)?;
         let sum_1 = &x.sum_all()?;
+        let mean_1 = &x.mean_all()?;
 
-        println!("{:?}", sum.shape().dims());
-        println!("{:?}", sum_1.shape().elem_count());
+        println!("{:?}", sum);
+        println!("{:?}", sum_1);
+
+        let el = Tensor::new(&[x.shape().elem_count() as f32], &d)?;
+        println!("{:?}", el);
+
+        let mean_2 = sum_1.broadcast_div(&el)?;
+        println!("{:?}", mean_1.to_scalar::<f32>());
+        println!("{:?}", mean_2.to_scalar::<f32>());
+
+
 
         // assert_eq!(sum.to_vec2::<f32>()?, vec![12f32, 15f32, 18f32, 21f32]);
         // assert_eq!(sum_1.to_vec1::<f32>()?, vec![12f32, 15f32, 18f32, 21f32]);
