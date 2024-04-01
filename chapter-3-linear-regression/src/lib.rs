c0acb253cb3c3acac0aae31bc39608eeccdb9c96
王雪 <broken.ice1314@gmail.com>
2024-04-01 21:54:14 +0800

    chore: benches test

--- chapter-3-linear-regression/src/lib.rs
+++ chapter-3-linear-regression/src/lib.rs
@@ -1,5 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! # 第三章：线性神经网络
+//!
+//! 1. 线性回归
+//!     1.1. 线性回归手动实现
+//!     1.2. 线性回归candle库实现
+//! 
+//! 2. softmax回归
+//!     2.1. softmax回归手动实现
+//!     2.2. softmax回归candle库实现
+//! 
+//!
+//! 
+#![feature(test)]
+extern crate test;
+
+// use candle_core::{Device, Result, Tensor};
+/// # 线性回归
+pub struct LinearRegression;
+
+impl LinearRegression {
 }
 
 #[cfg(test)]
@@ -8,7 +26,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // let result = add(2, 2);
+        // assert_eq!(result, 4);
     }
 }
